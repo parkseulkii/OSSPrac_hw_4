0656beaa56a6f006c364d70ff7f9d8f7c02fd3b1
parkseulkii <thsuwjstkdbf@naver.com>
2022-09-27 15:26:35 +0900

    2022-09-27 실습과제#4 3번

--- OSSPrac_hw_4-main/Main/src/App.java
+++ OSSPrac_hw_4-main/Main/src/App.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("이름을 입력하세요:");
+    String name = sc.next();
+
+    System.out.print("학번을 입력하세요:");
+    int num = sc.nextInt();
+    
+    System.out.println("");
+    System.out.println("<출력>");
+    System.out.println("이름:"+name);
+    System.out.println("학번:"+num);
+
+    sc.close();
+
     }
 }
