65306e8d342b229f79ecea720943a95a4c63241e
parkseulkii <thsuwjstkdbf@naver.com>
2022-10-04 15:41:44 +0900

    2022-10-04 실습과제#5 5번

--- OSSPrac_hw_4-main/Main/src/App.java
+++ OSSPrac_hw_4-main/Main/src/App.java
@@ -12,12 +12,16 @@ public class App {
 
     System.out.print("학과를 입력하세요:");
     String major = sc.next();
+
+    System.out.print("학년을 입력하세요:");
+    int grade = sc.nextInt();
     
     System.out.println("");
     System.out.println("<출력>");
     System.out.println("이름:"+name);
     System.out.println("학번:"+num);
     System.out.println("학과:"+major);
+    System.out.println("학년:"+grade);
     
 
     sc.close();
